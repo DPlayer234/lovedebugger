4c6c7995da790ad4cdc88622a3908e427fdf5445
DPlayer234 <dplayer.235@gmail.com>
2018-12-20 00:50:57 +0100

    Tiny tweak to pretty function name generation

--- debugger/func_index.lua
+++ debugger/func_index.lua
@@ -5,8 +5,14 @@ terms of the Do What The Fuck You Want To Public License, Version 2,
 as published by Sam Hocevar. See the COPYING file for more details.
 ]]
 return function(DBG)
+	local love_filesystem = require("love.filesystem")
 	local debug = require "debug"
 	local assert, setmetatable, next, type, ipairs, pairs, pcall, rawequal = assert, setmetatable, next, type, ipairs, pairs, pcall, rawequal
+
+	local isFile = function(path)
+		local info = love_filesystem.getInfo(path)
+		return info and info.type == "file"
+	end
 
 	local indexFunctions = false
 	local prettyFunctions = false
@@ -19,12 +25,6 @@ return function(DBG)
 	function DBG.allowFunctionIndex(prettyNames)
 		DBG.printInfo(":Allowing the indexing of functions! Access to indexing is only allowed within the command line.")
 		indexFunctions = true
-
-		local filesystem = require("love.filesystem")
-		local isFile = function(path)
-			local info = filesystem.getInfo(path)
-			return info and info.type == "file"
-		end
 
 		local upvalueIds = setmetatable({}, { __mode = "k" })
 
@@ -92,7 +92,7 @@ return function(DBG)
 				if isFile(source) then
 					local i = 0
 					local codelines = {}
-					for line in filesystem.lines(source) do
+					for line in love_filesystem.lines(source) do
 						i = i + 1
 						-- linedefined, lastlinedefined, params
 						if i >= info.linedefined then
@@ -150,125 +150,118 @@ return function(DBG)
 			--__metatable = false
 		}
 
-		if prettyNames and debug.getinfo then
-			prettyFunctions = true
-
-			local amount, bytes = 1, 5
-			local hardnames = {
-				[DBG.lua_print] = "print"
-			}
-
-			local indexed = {
-				[DBG._envRoot] = true,
-				[package.loaded] = true,
-				[package.preload] = true
-			}
-
-			local function addName(item, path)
-				if indexed[item] or hardnames[item] then return end
-				if type(item) == "table" then
-					indexed[item] = true
-					for k, v in next, item do
-						if type(k) == "string" then
-							addName(v, path .. "." .. k)
-						end
-					end
-				elseif type(item) == "function" then
-					hardnames[item] = path
-					amount = amount + 1
-					bytes = bytes + #path
-				end
-			end
-
-			for i,v in ipairs {
-				"assert", "collectgarbage", "dofile", "error", "gcinfo", "getfenv", "getmetatable", "ipairs", "load", "loadfile", "loadstring",
-				"module", "newproxy", "next", "pairs", "pcall", "rawequal", "rawget", "rawset", "require", "select",
-				"setfenv", "setmetatable", "type", "tonumber", "tostring", "unpack", "xpcall",
-				"coroutine", "debug", "io", "math", "os", "string", "table", "package"
-			} do
-				addName(DBG._envRoot[v], v)
-			end
-
-			for i,v in ipairs {
-				"bit", "jit", "love"
-			} do
-				local s, r = pcall(require, v)
-				if s then
-					addName(r, v)
-				end
-			end
-
-			do
-				local ffi = require "ffi"
-
-				addName(ffi, "ffi")
-				addName(debug.getmetatable(ffi.new("int")), "<cdata>")
-			end
-
-			addName(DBG, DBG._PATH)
-
-			local names = setmetatable({}, {
-				__index = function(t, f)
-					local v
-
-					local info = debug.getinfo(f, "S")
-					local source = (info.source or ""):gsub("^@", "")
-					local linedefined = info.linedefined
-					if isFile(source) and linedefined then
-						local i = 0
-						local defined
-						for line in filesystem.lines(source) do
-							i = i + 1
-							-- linedefined, lastlinedefined, params
-							if i >= linedefined then
-								defined = " "..line.." "
-								break
-							end
-						end
-						if defined then
-							v = defined:match("%)%-%-%[%[(.-)%]%]")
-								or defined:match("[^_a-zA-Z0-9]function%s+([_a-zA-Z][%.%:_a-zA-Z0-9]*)[^_a-zA-Z0-9]")
-								or defined:match("[^_a-zA-Z0-9]([_a-zA-Z][%.%:_a-zA-Z0-9]*)%s*=%s*%(*function[^_a-zA-Z0-9]")
-							if not v then
-								local __tostring = funcMeta.__tostring
-								funcMeta.__tostring = nil
-								v = DBG._tostring(f):match("0x%x+")
-								funcMeta.__tostring = __tostring
-							end
-						end
-					end
-
-					local shortSrc = info.short_src
-					local location =
-						shortSrc == "[C]" and (" [C]") or
-						(" (" .. shortSrc .. ":" .. DBG._tostring(linedefined) .. ")")
-
-					if v or hardnames[f] then
-						v = "function: " .. (hardnames[f] or v) .. location
-						if hardnames[f] then hardnames[f] = nil end
-					else
-						local __tostring = funcMeta.__tostring
-						funcMeta.__tostring = nil
-						v = DBG._tostring(f) .. location
-						funcMeta.__tostring = __tostring
-					end
-
-					t[f] = v
-					return v
-				end,
-				__mode = "kv"
-			})
-
-			function funcMeta:__tostring()
-				return names[self]
-			end
-
-			DBG.printInfo(":Added " .. DBG._tostring(amount) .. " function names for predefined functions, totalling " .. DBG._tostring(bytes) .. " characters.")
+		if prettyNames then
+			DBG._addPrettyFunctionNames(funcMeta)
 		else
 			prettyFunctions = false
 		end
 
 		debug.setmetatable(function() end, funcMeta)
+	end
+
+	-- Registers the pretty function name __tostring method
+	function DBG._addPrettyFunctionNames(funcMeta)
+		prettyFunctions = true
+
+		local amount, bytes = 1, 5
+		local hardnames = {
+			[DBG.lua_print] = "print"
+		}
+
+		local indexed = {
+			[DBG._envRoot] = true,
+			[package.loaded] = true,
+			[package.preload] = true
+		}
+
+		local function addName(item, path)
+			if indexed[item] or hardnames[item] then return end
+			if type(item) == "table" then
+				indexed[item] = true
+				for k, v in next, item do
+					if type(k) == "string" then
+						addName(v, path .. "." .. k)
+					end
+				end
+			elseif type(item) == "function" then
+				hardnames[item] = path
+				amount = amount + 1
+				bytes = bytes + #path
+			end
+		end
+
+		for i, v in ipairs {
+			"assert", "collectgarbage", "dofile", "error", "gcinfo", "getfenv", "getmetatable", "ipairs", "load", "loadfile", "loadstring",
+			"module", "newproxy", "next", "pairs", "pcall", "rawequal", "rawget", "rawset", "require", "select",
+			"setfenv", "setmetatable", "type", "tonumber", "tostring", "unpack", "xpcall",
+			"coroutine", "debug", "io", "math", "os", "string", "table", "package"
+		} do
+			addName(DBG._envRoot[v], v)
+		end
+
+		for i, v in ipairs {
+			"bit", "jit", "love", "ffi"
+		} do
+			local s, r = pcall(require, v)
+			if s then
+				addName(r, v)
+			end
+		end
+
+		addName(debug.getmetatable(DBG.fakeNil), "<cdata>")
+
+		addName(DBG, DBG._PATH)
+
+		local names = setmetatable({}, {
+			__index = function(t, f)
+				local name
+
+				local info = debug.getinfo(f, "S")
+				local source = (info.source or ""):gsub("^@", "")
+				local linedefined = info.linedefined
+				if linedefined == 0 then
+					return ("function: \"%s\""):format(source)
+				elseif isFile(source) and linedefined then
+					local i = 0
+					local defined
+					for line in love_filesystem.lines(source) do
+						i = i + 1
+						-- linedefined, lastlinedefined, params
+						if i >= linedefined then
+							defined = " "..line.." "
+							break
+						end
+					end
+					if defined then
+						name = defined:match("%)%-%-%[%[(.-)%]%]")
+							or defined:match("[^_a-zA-Z0-9]function%s+([_a-zA-Z][%.%:_a-zA-Z0-9]*)[^_a-zA-Z0-9]")
+							or defined:match("[^_a-zA-Z0-9]([_a-zA-Z][%.%:_a-zA-Z0-9]*)%s*=%s*%(*function[^_a-zA-Z0-9]")
+					end
+				end
+
+				local shortSrc = info.short_src
+				local location =
+					shortSrc == "[C]" and "[C]" or
+					("(" .. shortSrc .. ":" .. DBG._tostring(linedefined) .. ")")
+
+				if name or hardnames[f] then
+					name = ("function: %s %s"):format(hardnames[f] or name, location)
+					hardnames[f] = nil
+				else
+					name = ("function: %p %s"):format(f, location)
+				end
+
+				t[f] = name
+				return name
+			end,
+			__mode = "kv"
+		})
+
+		function funcMeta:__tostring()
+			return names[self]
+		end
+
+		DBG.printInfo(":Added " .. DBG._tostring(amount) .. " function names for predefined functions, totalling " .. DBG._tostring(bytes) .. " characters.")
 	end
 
 	-- Disallows function indexing
